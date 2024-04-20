7a360ec4802b75712b4eca6c1a9087838f1d7d07
Dmitrii Martynov <dmitrii.martynov@deptagency.com>
2024-04-20 15:19:04 +0700

    testing

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import uvicorn
 
 
-host="0.0.0.0"
-port=8002
-app_name="app.app:app"
+host = "0.0.0.0"
+port = 8002
+app_name = "app.app:app"
 
 
 if __name__ == '__main__':
     uvicorn.run(app_name, host=host, port=port, reload=True)
-    print('test line')
+    print('test lineads')
